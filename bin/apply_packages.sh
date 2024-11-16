3f3a2d2d5f0662d2cf606b29a079d65a21ae93a3
Christopher Ng <facboy@gmail.com>
2024-11-16 16:24:27 +0000

    Apply `knot-resolver` patch to `packages` while waiting for upstream merge

--- bin/apply_packages.sh
+++ bin/apply_packages.sh
@@ -5,4 +5,4 @@ script_dir="$(dirname "$(realpath "$0")")"
 
 . ${script_dir}/apply_common.sh
 
-(cd feeds/packages; patch -p1 -i ${arc_dir}/$FILESTAMP-packages.patch)
+(cd feeds/packages; patch -p1 -i ${arc_dir}/$FILESTAMP-packages.patch; git cherry-pick knot-resolver)
