228cc342644987be4e63c686d90ecb34532470bc
Christopher Ng <facboy@gmail.com>
2025-06-26 11:54:50 +0100

    Update to version R7800-main-r30100-143cfd6113-20250616-2302

--- R7800-main-newBuildroot.sh
+++ R7800-main-newBuildroot.sh
@@ -13,7 +13,7 @@ TARGET=main
 GITREPO=https://git.openwrt.org/openwrt/openwrt.git
 
 ## Current version
-FILESTAMP=R7800-main-r29772-1df56d02cf-20250525-2205
+FILESTAMP=R7800-main-r30100-143cfd6113-20250616-2302
 
 ### Prerequisites for buildroot for the current Ubuntu
 sudo apt-get install build-essential libncurses5-dev zlib1g-dev
